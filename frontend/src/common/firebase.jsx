af35938a7845fa9e60719d5e07567b9dfcaa2f1f
Satyajeet-01 <1968519685a@gmail.com>
2024-08-31 23:01:00 +0530

    initial commit

---
+++ frontend/src/common/firebase.jsx
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
+console.log(firebaseConfig)
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//google auth
+
+const provider = new GoogleAuthProvider();
+
+const auth = getAuth();
+
+export const authWithGoogle = async () => {
+  let user = null;
+  await signInWithPopup(auth, provider)
+    .then((result) => {
+      user = result.user;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  return user;
+};
